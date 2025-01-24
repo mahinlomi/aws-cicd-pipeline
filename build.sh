eba96cc0e62c42d0a5828f5fb2d728d1c76ed4a9
Marnito I. Mahinlo <mahinlo.mi@sprobe.com>
2025-01-24 16:53:11 +0800

    version 1.7

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #1/bin/bash
 
-docker build --tag node-express:latest .
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 124355646485.dkr.ecr.ap-southeast-1.amazonaws.com
+docker build -t cicd .
+docker tag cicd:latest 124355646485.dkr.ecr.ap-southeast-1.amazonaws.com/cicd:latest
+docker push 124355646485.dkr.ecr.ap-southeast-1.amazonaws.com/cicd:latest
